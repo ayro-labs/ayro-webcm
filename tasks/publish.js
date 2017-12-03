3fd6b8fe4d965ea48d93041725e739ba467247bf
Sandro Simas <sandro.csimas@gmail.com>
2017-12-02 21:46:08 -0300

    Improving publish task

--- tasks/publish.js
+++ tasks/publish.js
@@ -3,6 +3,7 @@ const utils = require('./utils');
 const path = require('path');
 const Promise = require('bluebird');
 
+const REPOSITORY_NAMESPACE = 'ayro';
 const REPOSITORY_URL = '554511234717.dkr.ecr.us-west-1.amazonaws.com';
 const WORKING_DIR = path.resolve(__dirname, '../');
 
@@ -27,16 +28,16 @@ function lintProject() {
 function buildImage() {
   return Promise.coroutine(function* () {
     utils.log('Building image...');
-    yield exec(`docker build -t ${projectPackage.name} .`);
+    yield exec(`docker build -t ${REPOSITORY_NAMESPACE}/${projectPackage.name} .`);
     utils.log('Tagging image...');
-    yield exec(`docker tag ${projectPackage.name}:latest ${REPOSITORY_URL}/${projectPackage.name}:latest`);
+    yield exec(`docker tag ${REPOSITORY_NAMESPACE}/${projectPackage.name}:latest ${REPOSITORY_URL}/${REPOSITORY_NAMESPACE}/${projectPackage.name}:latest`);
   })();
 }
 
 function publishToRegistry() {
   return Promise.coroutine(function* () {
     utils.log('Publishing to Registry...');
-    yield exec(`docker push ${REPOSITORY_URL}/${projectPackage.name}:latest`);
+    yield exec(`docker push ${REPOSITORY_URL}/${REPOSITORY_NAMESPACE}/${projectPackage.name}:latest`);
   })();
 }
 
