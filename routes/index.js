07943bf3c6c1e33dc6c0cc0b2ad7027233232d08
Sandro Simas <sandro.csimas@gmail.com>
2018-01-09 23:12:01 -0300

    Fixing webcm push

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ module.exports = (router, app, webSocket) => {
 
   app.post('/push/:user', (req, res) => {
     logger.debug('Publishing message of event %s to user %s', req.body.event, req.params.user);
+    const {message} = req.body;
+    message.date = message.date.toJSON();
     webSocket.getClient().publish(`/users/${req.params.user}`, req.body);
     res.sendStatus(200);
   });
