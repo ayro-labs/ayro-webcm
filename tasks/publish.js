911906372d712feeb465fab38d8bb00d022e3665
Sandro Simas <sandro.csimas@gmail.com>
2018-05-21 15:53:20 -0300

    Fixing publish task

--- tasks/publish.js
+++ tasks/publish.js
@@ -10,10 +10,15 @@ async function lintProject() {
   await commands.exec('npm run lint', WORKING_DIR);
 }
 
+async function buildProject() {
+  // Nothing to do...
+}
+
 // Run this if call directly from command line
 if (require.main === module) {
   publishTask.withWorkingDir(WORKING_DIR);
   publishTask.withLintTask(lintProject);
+  publishTask.withBuildTask(buildProject);
   publishTask.isDockerProject(true);
   publishTask.run();
 }
